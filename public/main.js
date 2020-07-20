073679987a89a51f43a997ba4f6a7ac346e117d1
nyambura00 <winnienyambura14@gmail.com>
2020-07-20 19:18:49 +0300

    modified JS functionality

--- public/main.js
+++ public/main.js
@@ -2,7 +2,7 @@ function myCountdown(){
 var birthday = new Date("July 19 2020, 00:00:00").getTime(); //birthday!
 
 //setting up my birthday function that runs after every second
-var birthdayFunction = setInterval(function(){
+const birthdayFunction = setInterval(function(){
 var currentTime = new Date().getTime();         //the Now time
 var timeLeft = birthday - currentTime;         //the time difference that exists in milliseconds
 
@@ -14,11 +14,13 @@ var seconds = Math.floor((timeLeft % (1000*60)) / 1000);
 
 //expressing content to the inner HTML
 document.getElementById("dynamic-content").innerHTML = days + "days : " + hours + "hrs : " + minutes + " mins :" + seconds + " secs ";
-
 //configuring a stopping function for the countdown
 if(timeLeft < 0){
 clearInterval(birthdayFunction);
-document.getElementById("end-text").innerHTML = "Happy 22nd Birthday Winnie! @19/07/2020!";
+const happyBirthday = document.getElementById("countdown-container");
+happyBirthday.innerHTML = "Happy 22nd Birthday Winnie! @19/07/2020!";
+happyBirthday.style.color = "white";
+happyBirthday.style.alignItems = "center"; //I'll be checking on this one
 }
 }, 1000);
 }
